e2aadf20a376b4315451b3a914d08809ba6530c4
Tayte Bitton <82904604+taytey@users.noreply.github.com>
2023-09-28 01:20:54 -0400

    initial

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// You might choose this based on an API call or logged-in status
+const navbar = "navbar";
+</script>
+<template>
+  <div>
+    <NuxtLayout :name="navbar">
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
